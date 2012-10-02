77701527bc92cb2cbf1d9aa12a7bd90aa807dbde
HIROE Shin <hiroe.orz@gmail.com>
2012-10-02 10:10:24 +0900

    http header value to lowercase.

--- src/org/java_websocket/drafts/Draft_76.java
+++ src/org/java_websocket/drafts/Draft_76.java
@@ -130,8 +130,11 @@ public class Draft_76 extends Draft_75 {
 
 	@Override
 	public HandshakeState acceptHandshakeAsServer( ClientHandshake handshakedata ) {
-		if( handshakedata.getFieldValue( "Upgrade" ).equals( "WebSocket" ) && handshakedata.getFieldValue( "Connection" ).contains( "Upgrade" ) && handshakedata.getFieldValue( "Sec-WebSocket-Key1" ).length() > 0 && !handshakedata.getFieldValue( "Sec-WebSocket-Key2" ).isEmpty() && handshakedata.hasFieldValue( "Origin" ) )
-			return HandshakeState.MATCHED;
+		if( handshakedata.getFieldValue( "Upgrade" ).toLowerCase( Locale.ENGLISH ).equals( "websocket" ) && 
+		    handshakedata.getFieldValue( "Connection" ).toLowerCase( Locale.ENGLISH ).contains( "upgrade" ) && 
+		    handshakedata.getFieldValue( "Sec-WebSocket-Key1" ).length() > 0 && 
+		    !handshakedata.getFieldValue( "Sec-WebSocket-Key2" ).isEmpty() && handshakedata.hasFieldValue( "Origin" ) )
+		  return HandshakeState.MATCHED;
 		return HandshakeState.NOT_MATCHED;
 	}
 
